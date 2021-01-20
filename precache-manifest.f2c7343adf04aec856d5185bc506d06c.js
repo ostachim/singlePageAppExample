faa1bcaf98a80ea3c6c7a017d3f765dd1b52ec3c
Michal O <ostachim98@gmail.com>
2021-01-20 16:33:11 +0100

    Updates

---
+++ precache-manifest.f2c7343adf04aec856d5185bc506d06c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "44554496a70042613179",
+    "url": "/singlePageAppExample/static/css/main.d60b7b5b.chunk.css"
+  },
+  {
+    "revision": "44554496a70042613179",
+    "url": "/singlePageAppExample/static/js/main.86f1076a.chunk.js"
+  },
+  {
+    "revision": "6c701d794255401d645e",
+    "url": "/singlePageAppExample/static/js/runtime~main.c7e2441b.js"
+  },
+  {
+    "revision": "e2fd5b278fc3d82e9cbd",
+    "url": "/singlePageAppExample/static/js/2.7e9e480d.chunk.js"
+  },
+  {
+    "revision": "3bee94ea8e9dd4beb8360f1badab5e8d",
+    "url": "/singlePageAppExample/static/media/header1.3bee94ea.jpg"
+  },
+  {
+    "revision": "459d83344583205e3497d43048900164",
+    "url": "/singlePageAppExample/static/media/header2.459d8334.jpg"
+  },
+  {
+    "revision": "f463926cd7be8b2e4b75cb714e79cc3b",
+    "url": "/singlePageAppExample/static/media/header3.f463926c.jpg"
+  },
+  {
+    "revision": "6b25512ef96d40903f1a7b69194ff3bc",
+    "url": "/singlePageAppExample/index.html"
+  }
+];
